a3a6d550b8a82cac0e9178e7533de3de2db142df
Lnurhalim <69791691+datahalim@users.noreply.github.com>
2026-05-29 09:10:58 +0700

    Update project attribution in 9latian9.php

--- Pert-9/9latian9.php
+++ Pert-9/9latian9.php
@@ -4,5 +4,5 @@ function hitungLuas($panjang, $lebar) {
 }
 
 echo hitungLuas(5, 3); // Output: 15
-// Project 9.2 By AKBAR FAUZI MUTAQIN - 221011450673
+// Project 9.2 By LILI NURHALIM - 221011450310
 ?>
